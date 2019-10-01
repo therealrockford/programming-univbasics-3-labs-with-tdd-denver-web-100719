ac4d1a1c5265c654edbe59e27f3a1959ff026a9a
Rocky Pitto <rockypitto@gmail.com>
2019-10-01 19:29:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,4 +5,4 @@ second_number = 2.0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number % second_number
+quotient = first_number / second_number
